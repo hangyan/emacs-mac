3b505227227f07c2046c025e6179c9c11b015079
Hang Yan <yhang@vmware.com>
2024-09-12 16:20:27 +0800

    update:
    Signed-off-by: Hang Yan <yhang@vmware.com>

---
+++ init.el
@@ -0,0 +1,73 @@
+
+
+
+;; full path
+(setq frame-title-format
+      (list (format "%s %%S: %%j " (system-name))
+        '(buffer-file-name "%f" (dired-directory dired-directory "%b"))))
+
+
+(setq inhibit-startup-message t)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+
+(setq mac-command-modifier 'meta)
+
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+
+; path settings
+(defun set-exec-path-from-shell-PATH ()
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string
+                          "[ \t\n]*$" "" (shell-command-to-string
+                                          "$SHELL --login -c 'echo $PATH'"
+                                                    ))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+(set-exec-path-from-shell-PATH)
+
+
+;; indent-guide
+(indent-guide-global-mode)
+(global-display-line-numbers-mode)
+(global-hl-line-mode t)
+
+;; helm
+(require 'helm)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(helm-mode 1)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(setq helm-xref-candidate-formatting-function 'helm-xref-format-candidate-full-path)
+(recentf-mode 1)
+(setq-default recent-save-file "~/.emacs.d/recentf")
+;;(helm-recentf)
+(define-key helm-map (kbd "TAB") #'helm-execute-persistent-action)
+(define-key helm-map (kbd "<tab>") #'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-z") #'helm-select-action)
+(global-set-key (kbd "C-x C-b") 'recentf-open)
+
+
+
+(defconst *is-a-mac* (eq system-type 'darwin))
+(defconst *is-a-win* (eq system-type 'windows-nt))
+
+(setq make-backup-files nil)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(tango-dark))
+ '(package-selected-packages '(exec-path-from-shell helm)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "UbuntuMono Nerd Font Mono" :foundry "nil" :slant normal :weight regular :height 160 :width normal)))))
